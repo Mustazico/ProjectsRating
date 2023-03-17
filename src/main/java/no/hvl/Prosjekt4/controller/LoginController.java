e838c6b0c175d10e8d8f5156a9d453e6cf143a10
h591168 <591168@stud.hvl.no>
2023-03-17 15:24:42 +0100

    testing

--- src/main/java/no/hvl/Prosjekt4/controller/LoginController.java
+++ src/main/java/no/hvl/Prosjekt4/controller/LoginController.java
@@ -1,5 +1,25 @@
 package no.hvl.Prosjekt4.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping(value = "login")
 public class LoginController {
 
+    @PostMapping("/login")
+    public String login(@RequestParam String brukernavn, @RequestParam String passord) {
+
+        // Sjekker om passord og brukernavn er riktig
+
+        if (brukernavn.equals("admin") && passord.equals("passord")) {
+            return "Innlogget!";
+        } else {
+            return "Feil brukernavn eller passord";
+        }
+    }
 }
