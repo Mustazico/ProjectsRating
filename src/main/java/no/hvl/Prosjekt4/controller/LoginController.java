5c04c1ca76815bfff3162a1044d03f850ea455b9
Torbenhjl <torbenhjl.50@gmail.com>
2023-03-20 11:56:27 +0100

    Yoooooooooo

--- src/main/java/no/hvl/Prosjekt4/controller/LoginController.java
+++ src/main/java/no/hvl/Prosjekt4/controller/LoginController.java
@@ -1,5 +1,30 @@
 package no.hvl.Prosjekt4.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/logginn")
 public class LoginController {
+
+    @GetMapping
+    public String visLogginn() {
+        return "logginn";
+    }
+
+    @PostMapping
+    public String loggInn(@RequestParam(name = "username") String brukernavn,
+            @RequestParam(name = "password") String passord, RedirectAttributes ra) {
+        if (brukernavn.equals("admin") && passord.equals("passord")) {
+            ra.addFlashAttribute("msg", "Du er logget inn");
+            return "redirect:" + "landingpage";
+        }
+        ra.addFlashAttribute("msg", "Feil brukernavn eller passord");
+        return "redirect:" + "logginn";
+    }
 
 }
--- src/main/java/no/hvl/Prosjekt4/entity/Brukere.java
+++ src/main/java/no/hvl/Prosjekt4/entity/Brukere.java
@@ -1,5 +1,40 @@
+
 package no.hvl.Prosjekt4.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "prosjekt4")
 public class Brukere {
+
+    @Id
+    private String brukernavn;
+    private String passord;
+
+    public Brukere() {
+    }
+
+    public Brukere(String brukernavn, String passord) {
+        this.brukernavn = brukernavn;
+        this.passord = passord;
+    }
+
+    public String getBrukernavn() {
+        return brukernavn;
+    }
+
+    public void setBrukernavn(String brukernavn) {
+        this.brukernavn = brukernavn;
+    }
+
+    public String getPassord() {
+        return passord;
+    }
+
+    public void setPassord(String passord) {
+        this.passord = passord;
+    }
 
 }
