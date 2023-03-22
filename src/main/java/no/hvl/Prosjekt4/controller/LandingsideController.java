bb066e6e8341d17118f54b31bcbbf15e4547eb5e
h596134 <596134@stud.hvl.no>
2023-03-22 15:03:44 +0100

    skal merge

--- src/main/java/no/hvl/Prosjekt4/controller/LandingsideController.java
+++ src/main/java/no/hvl/Prosjekt4/controller/LandingsideController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.*;
+
+import no.hvl.Prosjekt4.util.BrukerService;
 import no.hvl.Prosjekt4.util.JPARepo;
 import no.hvl.Prosjekt4.util.LoginUtil;
 
