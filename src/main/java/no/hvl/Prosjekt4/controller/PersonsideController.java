a36850aea1980dad4aa1e24f30b2ac58578bb5ae
Torbenhjl <torbenhjl.50@gmail.com>
2023-03-22 12:54:49 +0100

    legger til personside

---
+++ src/main/java/no/hvl/Prosjekt4/controller/PersonsideController.java
@@ -0,0 +1,29 @@
+package no.hvl.Prosjekt4.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import no.hvl.Prosjekt4.entity.Brukere;
+import no.hvl.Prosjekt4.util.JPARepo;
+
+@RestController
+@RequestMapping("/personside")
+public class PersonsideController {
+
+    @Autowired
+    private JPARepo brukerRepo;
+
+    @GetMapping
+    public ResponseEntity<Brukere> visPersonside(@PathVariable String brukernavn) {
+        Brukere bruker = brukerRepo.findByBrukernavn(brukernavn);
+        if (bruker == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(bruker);
+    }
+
+}
