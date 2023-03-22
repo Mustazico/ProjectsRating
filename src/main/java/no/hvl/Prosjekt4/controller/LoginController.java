8a4da411c4c469095794f7ca85bfa9eff366c268
h596134 <596134@stud.hvl.no>
2023-03-22 13:37:03 +0100

    kjlkjljij

--- src/main/java/no/hvl/Prosjekt4/controller/LoginController.java
+++ src/main/java/no/hvl/Prosjekt4/controller/LoginController.java
@@ -1,16 +1,25 @@
 package no.hvl.Prosjekt4.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import no.hvl.Prosjekt4.util.JPARepo;
+import no.hvl.Prosjekt4.util.LoginUtil;
 
 @Controller
 @RequestMapping("/logginn")
 public class LoginController {
 	
+	@Autowired
+	JPARepo brukerrepo;
 
     @GetMapping
     public String visLogginn() {
@@ -18,10 +27,15 @@ public class LoginController {
     }
 
     @PostMapping
-    public String loggInn(@RequestParam(name = "username") String brukernavn,
-            @RequestParam(name = "password") String passord, RedirectAttributes ra) {
-        if (brukernavn.equals("admin") && passord.equals("passord")) {
+    public String loggInn(
+    		HttpServletRequest request, 
+    		@RequestParam(name = "username") String brukernavn,
+            @RequestParam(name = "password") String passord,
+            RedirectAttributes ra) {
+    	
+        if (LoginUtil.rettPassord(brukerrepo, brukernavn, passord)) {
             ra.addFlashAttribute("msg", "Du er logget inn");
+            LoginUtil.loggInnBruker(request, brukernavn, passord);
             return "redirect:" + "landingpage";
         }
         ra.addFlashAttribute("msg", "Feil brukernavn eller passord");
