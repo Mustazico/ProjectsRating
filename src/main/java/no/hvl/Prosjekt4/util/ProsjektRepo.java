b199ec5db9980cfc56c2d5db42b9c909106bbb4d
h594754 <594754@stud.hvl.no>
2023-03-24 11:52:53 +0100

    Gjør klart for merge

--- src/main/java/no/hvl/Prosjekt4/util/ProsjektRepo.java
+++ src/main/java/no/hvl/Prosjekt4/util/ProsjektRepo.java
@@ -3,11 +3,12 @@ package no.hvl.Prosjekt4.util;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import no.hvl.Prosjekt4.entity.Prosjektliste;
 
 public interface ProsjektRepo extends JpaRepository<Prosjektliste, Integer>{
-	List<Prosjektliste> findByBrukerid(String id); 
 	Prosjektliste findByTittel(String tittel);
 	Prosjektliste findByProsjektlink(String prosjektlink);
 	
