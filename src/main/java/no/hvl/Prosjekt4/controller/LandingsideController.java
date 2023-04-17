e1dfecee7443165f951533a2c50328b3c795d3c1
Kristoffer Madsen <599017@stud.hvl.no>
2023-04-17 10:33:45 +0200

    hj

--- src/main/java/no/hvl/Prosjekt4/controller/LandingsideController.java
+++ src/main/java/no/hvl/Prosjekt4/controller/LandingsideController.java
@@ -22,9 +22,12 @@ import no.hvl.Prosjekt4.util.ProsjektRepo;
 import no.hvl.Prosjekt4.util.ProsjektService;
 import no.hvl.Prosjekt4.util.RatingRepo;
 import no.hvl.Prosjekt4.util.RatingsUtil;
+
 /**
- * Dette er controlleren for landingssiden til brukeren. Den er ansvarlig for å håndtere forespørsler knyttet til landingssiden.
- * Landingssiden er den siden brukeren kommer til når han logger inn. Den inneholder informasjon om brukeren og prosjektene han er med i.
+ * Dette er controlleren for landingssiden til brukeren. Den er ansvarlig for å
+ * håndtere forespørsler knyttet til landingssiden. Landingssiden er den siden
+ * brukeren kommer til når han logger inn. Den inneholder informasjon om
+ * brukeren og prosjektene han er med i.
  */
 @Controller
 @RequestMapping(value = "/landingpage", produces = "text/html;charset=UTF-8")
@@ -49,116 +52,114 @@ public class LandingsideController {
 
 	@Autowired
 	private RatingRepo ratingRepo;
-/**
- * Denne metoden håndterer forespørsler om landingssiden. Den tar inn en model, en session og en request. Den returnerer en String som er navnet på html-siden som skal vises.
- * @param model er en model som inneholder informasjon som skal vises på siden.
- * @param session er en session som inneholder informasjon om brukeren som er logget inn.
- * @param request er en request som inneholder informasjon om forespørselen.
- * @return String som er navnet på html-siden som skal vises.
- * @throws Exception kan kaste en exception.
-*/
+
+	/**
+	 * Denne metoden håndterer forespørsler om landingssiden. Den tar inn en model,
+	 * en session og en request. Den returnerer en String som er navnet på
+	 * html-siden som skal vises.
+	 * 
+	 * @param model   er en model som inneholder informasjon som skal vises på
+	 *                siden.
+	 * @param session er en session som inneholder informasjon om brukeren som er
+	 *                logget inn.
+	 * @param request er en request som inneholder informasjon om forespørselen.
+	 * @return String som er navnet på html-siden som skal vises.
+	 * @throws Exception kan kaste en exception.
+	 */
 	@GetMapping
 	@Transactional
 	public String visLandingpage(Model model, HttpSession session, HttpServletRequest request) throws Exception {
 
+		RatingsUtil ratingsutil = new RatingsUtil();
 
-			RatingsUtil ratingsutil = new RatingsUtil();
+		String id = "1";
+		model.addAttribute("id", id);
+		int newId = Integer.parseInt(id);
+		model.addAttribute(brukerRepo.findById(newId));
 		
-			String id = "1";
-			model.addAttribute("id", id);
-			int newId = Integer.parseInt(id);
-			model.addAttribute(brukerRepo.findById(newId));
-			List<String> lenker = ratingsutil.highestRatedForAll(pr, 8)
-					.stream()
-					.map(x->x.getBrukerid())
-					.collect(Collectors.toList());
-			
-			model.addAttribute("profilbilde", brukerRepo.getProfilbilde(newId));
-			model.addAttribute("lenker", lenker);
-			
-			
-			List<String> prosjektIdListe = ratingsutil.highestRatedForAll(pr, 8)
-					.stream()
-					.map(x->x.getProsjektid())
-					.collect(Collectors.toList());
-			List<String> test = new ArrayList<>();
-			List<String> githubbrukernavn = new ArrayList<>();
-			List<String> repo = new ArrayList<>();
-			
-			for (String s : prosjektIdListe) {
+		List<String> lenker = ratingsutil
+				.highestRatedForAll(pr, 8).stream()
+				.map(x -> x.getBrukerid())
+				.collect(Collectors.toList());
 
-				try {
-					test.add(api.kallReadMeApi(s));
-					githubbrukernavn.add(splitBrukernavn(s));
-					repo.add(splitRepo(s));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			
-			
+		model.addAttribute("profilbilde", brukerRepo.getProfilbilde(newId));
+		model.addAttribute("lenker", lenker);
 
-			model.addAttribute("githubBrukernavn", githubbrukernavn);
-			model.addAttribute("githubRepo", repo);
-			model.addAttribute("api", test);
-			model.addAttribute("bio", brukerRepo.getBrukerintro(newId));
-
-			List<String> prosjektidListe = new ArrayList<>();
-			List<String> brukernavnListe1 = ratingsutil.highestRatedForAll(pr, 8)
-					.stream()
-					.map(x->x.getProsjektid())
-					.collect(Collectors.toList());
-			List<String> brukernavnListe = new ArrayList<>();
-			
-			for(String idd : brukernavnListe1) {
-				brukernavnListe.add(brukerService.getBrukernavnByProsjektId(idd));
-			}
-			
-			
-			List<Prosjektliste> prosjekt = prosjektRepo.findAll();
+		List<String> prosjektIdListe = ratingsutil
+				.highestRatedForAll(pr, 8)
+				.stream()
+				.map(x -> x.getProsjektid())
+				.collect(Collectors.toList());
 		
-			//Prosjektliste prosjeket = new Prosjektliste();
-			
-			 List<String> gjennomsnittrating = new ArrayList<>();
-			 
-			 for(String s : brukernavnListe1) {
-				 Prosjektliste p = prosjektRepo.findByProsjektid(s);
-				 gjennomsnittrating.add(p.getGjennomsnittrating());
-			 }
-			 model.addAttribute("gjsnitt", gjennomsnittrating);
-			 
+		List<String> test = new ArrayList<>();
+		List<String> githubbrukernavn = new ArrayList<>();
+		List<String> repo = new ArrayList<>();
 
-			for (Prosjektliste p : prosjekt) {
-				prosjektidListe.add(prosjektRepo.findProsjektid(p));
-		//		 individuelle prosjekt - finn en måte å hente ut brukernavnet til prosjektet
-			
+		for (String s : prosjektIdListe) {
+
+			try {
+				test.add(api.kallReadMeApi(s));
+				githubbrukernavn.add(splitBrukernavn(s));
+				repo.add(splitRepo(s));
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			model.addAttribute("prosjektId", prosjektidListe);
-			model.addAttribute("brukernavn", brukernavnListe);
-			
-			
-		
+		}
+
+		model.addAttribute("githubBrukernavn", githubbrukernavn);
+		model.addAttribute("githubRepo", repo);
+		model.addAttribute("api", test);
+		model.addAttribute("bio", brukerRepo.getBrukerintro(newId));
+
+		List<String> prosjektidListe = new ArrayList<>();
+		List<String> brukernavnListe1 = ratingsutil.highestRatedForAll(pr, 8).stream().map(x -> x.getProsjektid())
+				.collect(Collectors.toList());
+		List<String> brukernavnListe = new ArrayList<>();
+
+		for (String idd : brukernavnListe1) {
+			brukernavnListe.add(brukerService.getBrukernavnByProsjektId(idd));
+		}
+
+		List<Prosjektliste> prosjekt = prosjektRepo.findAll();
+
+		List<String> gjennomsnittrating = new ArrayList<>();
+
+		for (String s : brukernavnListe1) {
+			Prosjektliste p = prosjektRepo.findByProsjektid(s);
+			gjennomsnittrating.add(p.getGjennomsnittrating());
+		}
+		model.addAttribute("gjsnitt", gjennomsnittrating);
+
+		for (Prosjektliste p : prosjekt) {
+			prosjektidListe.add(prosjektRepo.findProsjektid(p));
+			// individuelle prosjekt - finn en måte å hente ut brukernavnet til prosjektet
+
+		}
+		model.addAttribute("prosjektId", prosjektidListe);
+		model.addAttribute("brukernavn", brukernavnListe);
 
 		return "landingpage";
 	}
+
 	/**
 	 * Denne metoden brukes til å splitte GitHub brukernavn fra prosjekt linken.
+	 * 
 	 * @param id er prosjekt id'en.
 	 * @return String som er GitHub brukernavnet.
 	 * @throws Exception kan kaste en exception.
 	 */
-	
-	   public String splitBrukernavn(String id) {
-	    	String lenke = prosjektRepo.findProsjektidProsjektlink(id);
-			String[] deler = lenke.split("/");
-			String brukernavn = deler[3];
-	    	return brukernavn;
-	    }
-	    
-	    public String splitRepo(String id) {
-	    	String lenke = prosjektRepo.findProsjektidProsjektlink(id);
-			String[] deler = lenke.split("/");
-			String repo = deler[4];
-	    	return repo;
-	    }
+
+	public String splitBrukernavn(String id) {
+		String lenke = prosjektRepo.findProsjektidProsjektlink(id);
+		String[] deler = lenke.split("/");
+		String brukernavn = deler[3];
+		return brukernavn;
+	}
+
+	public String splitRepo(String id) {
+		String lenke = prosjektRepo.findProsjektidProsjektlink(id);
+		String[] deler = lenke.split("/");
+		String repo = deler[4];
+		return repo;
+	}
 }
